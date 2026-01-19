3c1d08229f11a969ef31c4273de9256c47e35d5f
julie <julie.lechartier@gmail.com>
2026-01-19 16:48:00 +0100

    [ADD] template show event

--- src/Controller/EventController.php
+++ src/Controller/EventController.php
@@ -29,8 +29,30 @@ final class EventController extends AbstractController
     #[Route('/show/{id}', name: 'app_event_show')]
     public function show($id): Response
     {
-        return $this->render('event/show.html.twig', [
 
+        $events = [
+            ['id' => 1, 'nom' => 'Soirée Networking Lyon', 'description' => 'Rencontrez les meilleurs professionnels du numérique autour d\'un cocktail dînatoire.', 'date' => new \DateTime('+2 weeks'), 'lieu' => 'Lyon 2e', 'prix' => 29, 'type' => 'Networking'],
+            ['id' => 2, 'nom' => 'Workshop Symfony Avancé', 'description' => 'Maîtrisez GraphQL et microservices avec Symfony 7.', 'date' => new \DateTime('+1 month'), 'lieu' => 'Lyon 3e', 'prix' => 89, 'type' => 'Formation'],
+            ['id' => 3, 'nom' => 'Concert Électro Premium', 'description' => 'Live DJ set dans un lieu mythique avec open bar.', 'date' => new \DateTime('+3 weeks'), 'lieu' => 'Lyon 1er', 'prix' => 45, 'type' => 'Concert'],
+            ['id' => 4, 'nom' => 'Afterwork Startup', 'description' => 'Pitchs innovants et networking entre fondateurs.', 'date' => new \DateTime('tomorrow'), 'lieu' => 'Part-Dieu', 'prix' => 15, 'type' => 'Afterwork'],
+            ['id' => 5, 'nom' => 'Gala Art & Tech', 'description' => 'Exposition immersive et dîner spectacle exclusif.', 'date' => new \DateTime('+1 week'), 'lieu' => 'Confluence', 'prix' => 125, 'type' => 'Gala'],
+            ['id' => 6, 'nom' => 'Hackathon 24h', 'description' => 'Codez votre solution avec mentors experts.', 'date' => new \DateTime('+4 weeks'), 'lieu' => 'Lyon 7e', 'prix' => 0, 'type' => 'Hackathon'],
+        ];
+
+        $event = null;
+        foreach ($events as $e) {
+            if ($e['id'] == $id) {
+                $event = $e;
+                break;
+            }
+        }
+
+        if (!$event) {
+            throw $this->createNotFoundException('Événement non trouvé');
+        }
+
+        return $this->render('event/show.html.twig', [
+            'event' => $event,
         ]);
     }
 
