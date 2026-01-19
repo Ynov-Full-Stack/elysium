cabd275abe38ebbaae332b6a22662038623075d0
julie <julie.lechartier@gmail.com>
2026-01-19 15:44:34 +0100

    [ADD] Alpine.js

--- assets/app.js
+++ assets/app.js
@@ -1,4 +1,7 @@
 import './stimulus_bootstrap.js';
 import './styles/app.css';
 
-console.log('This log comes from assets/app.js - welcome to AssetMapper! 🎉');
+import Alpine from 'alpinejs';
+
+window.Alpine = Alpine;
+Alpine.start();
