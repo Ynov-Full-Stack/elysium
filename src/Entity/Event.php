be49a478075176f1f3ee000af8c92e1cf134321f
Kenzo Thomias <thomiaskenzo01@gmail.com>
2026-01-19 15:59:37 +0100

    Attempt to create admin.

---
+++ src/Entity/Event.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\UserRepository;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
+use Symfony\Component\Security\Core\User\UserInterface;
+
+#[ORM\Entity(repositoryClass: UserRepository::class)]
+#[ORM\Table(name: 'event')]
+class Event {
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 180)]
+    private ?string $name = null;
+
+    #[ORM\Column]
+    private ?int $placeNumber = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): static
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getPlaceNumber(): ?int
+    {
+        return $this->placeNumber;
+    }
+
+    public function setPlaceNumber(int $placeNumber): static
+    {
+        $this->placeNumber = $placeNumber;
+
+        return $this;
+    }
+}
