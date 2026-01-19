a24d4d8ec4189cfe5e3233400bb184b587080028
Axel <axel@mac.home>
2026-01-19 16:17:58 +0100

    Add 403 and 404 files

--- assets/app.js
+++ assets/app.js
@@ -1,4 +1,3 @@
 import './stimulus_bootstrap.js';
 import './styles/app.css';
 
-console.log('This log comes from assets/app.js - welcome to AssetMapper! 🎉');
--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -11,6 +11,6 @@ final class DefaultController extends AbstractController
     #[Route('/', name: 'app_default')]
     public function index(): Response
     {
-        return $this->render('default/index.html.twig');
+        return $this->render('pages/default/index.html.twig');
     }
 }
