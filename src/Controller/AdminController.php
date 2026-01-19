437e8bbcfc9aea67dcc84bd9bc1ea733fff46462
Axel <axel@mac.home>
2026-01-19 17:06:49 +0100

    fix somes event errors then add templates

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Entity\Event;
 use App\Form\EventType;
+use App\Repository\EventRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -15,33 +16,37 @@ use Symfony\Component\Security\Http\Attribute\IsGranted;
 #[Route('/admin')]
 #[IsGranted('ROLE_ADMIN')]
 final class AdminController extends AbstractController {
-    private EntityManagerInterface $entityManager;
 
-    public function __construct(EntityManagerInterface $entityManager) {
-        $this->entityManager = $entityManager;
+    #[Route('/events', name: 'event_index', methods: ['GET'])]
+    public function index(EventRepository $eventRepository): Response
+    {
+        $events = $eventRepository->findAll();
+        return $this->render('pages/admin/index.html.twig', [
+            'events' => $events,
+        ]);
     }
 
     #[Route(path: '/event/new', name: 'event_add', methods: ['GET', 'POST'])]
-    public function add_event(Request $request): Response {
+    public function add_event(Request $request, EntityManagerInterface $entityManager): Response {
         $event = new Event();
         $form = $this->createForm(EventType::class, $event);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
-            $this->entityManager->persist($event);
-            $this->entityManager->flush();
+            $entityManager->persist($event);
+            $entityManager->flush();
 
             return $this->redirectToRoute('event_index', [], Response::HTTP_SEE_OTHER);
         }
 
-        return $this->render('admin/create.html.twig', [
+        return $this->render('pages/admin/create.html.twig', [
             'form' => $form->createView(),
             'controller_name' => 'AdminController',
         ]);
     }
-    
-    #[Route(path: '/event/{id}', name: 'event_delete', methods: ["DELETE"])]
-    public function remove_event(Request $request, Event $event): Response {
+
+    #[Route(path: '/event/{id}', name: 'event_delete', methods: ["POST"])]
+    public function remove_event(Event $event, Request $request, EntityManagerInterface $entityManager): Response {
         if ($this->isCsrfTokenValid('delete' . $event->getId(), $request->getPayload()->getString('_token'))) {
             $entityManager->remove($event);
             $entityManager->flush();
@@ -49,20 +54,22 @@ final class AdminController extends AbstractController {
         return $this->redirectToRoute('event_index', [], Response::HTTP_SEE_OTHER);
 
     }
-    
-    #[Route(path: '/event/{id}', name: 'event_edit', methods: ["PUT"])]
-    public function index(Request $request ,Event $event): Response {
+
+    #[Route('/event/{id}', name: 'event_edit', methods: ['GET', 'POST'])]
+    public function edit(Event $event, Request $request, EntityManagerInterface $entityManager
+    ): Response {
         $form = $this->createForm(EventType::class, $event);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
-            $this->entityManager->flush();
-
-            return $this->redirectToRoute('event_index', [], Response::HTTP_SEE_OTHER);
+            $entityManager->persist($event);
+            $entityManager->flush();
+            return $this->redirectToRoute('event_index');
         }
-        return $this->render('admin/index.html.twig', [
-			'form' => $form->createView(),
-            'controller_name' => 'AdminController',
+        return $this->render('pages/admin/edit.html.twig', [
+            'form' => $form->createView(),
+            'event' => $event,
         ]);
     }
+
 }
