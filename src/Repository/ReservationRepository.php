0c44ee2d7492de936c2e21448819639e67ac8b40
Kenzo Thomias <thomiaskenzo01@gmail.com>
2026-01-22 15:54:05 +0100

    Chore: Contenerize Mailer.

--- src/Repository/ReservationRepository.php
+++ src/Repository/ReservationRepository.php
@@ -3,6 +3,7 @@
 namespace App\Repository;
 
 use App\Entity\Reservation;
+use DateTimeZone;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
 
@@ -14,6 +15,27 @@ class ReservationRepository extends ServiceEntityRepository
     public function __construct(ManagerRegistry $registry)
     {
         parent::__construct($registry, Reservation::class);
+    }
+
+    public function findForReminder(
+        \DateInterval $interval,
+        string $offsetKey,
+        int $toleranceMinutes = 5
+    ): array {
+        $now = new \DateTimeImmutable();
+        
+        $targetTime = $now->add($interval);
+
+        $start = $targetTime->sub(new \DateInterval("PT{$toleranceMinutes}M"));
+        $end   = $targetTime->add(new \DateInterval("PT{$toleranceMinutes}M"));
+        
+        return $this->createQueryBuilder('r')
+            ->join('r.event', 'e')
+            ->where('e.eventDate BETWEEN :start AND :end')
+            ->setParameter('start', $start)
+            ->setParameter('end', $end)
+            ->getQuery()
+            ->getResult();
     }
 
     //    /**
