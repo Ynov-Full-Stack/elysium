0df48595db1a444ca8c07b369f396526c8ec5b53
Kenzo Thomias <thomiaskenzo01@gmail.com>
2026-01-22 10:53:11 +0100

    Feat: Adding mass email sending

--- src/Mail/MailService.php
+++ src/Mail/MailService.php
@@ -14,4 +14,36 @@ final class MailService
     {
         $this->bus->dispatch($message);
     }
+
+
+    // Usage exemple:
+    // $messages = $this->mailService->buildMessages(
+    //     users: $userRepository->findAdmins();,
+    //     factory: [MailMessage::class, 'wantedMailBuilder'],
+    //     args: [
+    //         arg1,
+    //         arg2,
+    //     ]
+    // );
+    // $this->mailService->sendToMany($messages);
+
+    public function buildMessages(iterable $users, callable $factory, array $args = []): array {
+        $messages = [];
+
+        foreach ($users as $user) {
+            $messages[] = $factory(
+                $user,
+                ...$args
+            );
+        }
+
+        return $messages;
+    }
+
+    public function sendToMany(iterable $messages): void
+    {
+        foreach ($messages as $message) {
+            $this->send($message);
+        }
+    }
 }
