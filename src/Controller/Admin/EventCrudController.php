b9f0692a7b86d68fc638597637d4f7d5998b2cfc
julie <julie.lechartier@gmail.com>
2026-01-22 09:34:28 +0100

    [ADD] event admin configure field

--- src/Controller/Admin/EventCrudController.php
+++ src/Controller/Admin/EventCrudController.php
@@ -4,6 +4,7 @@ namespace App\Controller\Admin;
 
 use App\Entity\Event;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\DateField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
@@ -15,14 +16,19 @@ class EventCrudController extends AbstractCrudController
         return Event::class;
     }
 
-    /*
     public function configureFields(string $pageName): iterable
     {
         return [
-            IdField::new('id'),
-            TextField::new('title'),
-            TextEditorField::new('description'),
+            TextField::new('name')
+            ->setLabel("Nom de l'événement"),
+            TextEditorField::new('description')
+            ->setLabel("Description"),
+            DateField::new('eventDate')
+            ->setLabel("Date de l'événement"),
+            DataField::new('registrationStartDate')
+            ->setLabel("Date d'ouverture des")
+
         ];
     }
-    */
+
 }
